f3bd80426ad98d626fd74112f0698cb4d04d9193
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-02-16 16:57:14 -0300

    seccion switch

---
+++ 01-fundamentos/assets/js/switch.js
@@ -0,0 +1,20 @@
+
+
+
+const dia = 2; // 0=dom, 1=lunes...etc
+
+switch (dia) {
+    case 0:
+        console.log('domingo');
+        break;
+    case 1:
+        console.log('Lunes');
+        break;
+    case 2:
+        console.log('Martes');
+        break;
+    default:
+        console.log('no es dom, lunes ni martes');
+        break;
+}
+
