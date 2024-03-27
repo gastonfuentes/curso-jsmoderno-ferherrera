76fa79af0ce97e470843f6bcef608ae5f94d149d
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-03-27 10:31:32 -0300

    validaciones y optimizaciones

--- 05-todo-app/src/todos/use-cases/create-todo-html.js
+++ 05-todo-app/src/todos/use-cases/create-todo-html.js
@@ -9,12 +9,26 @@ export const createTodoHtml = (todo) => {
     if (!todo) throw new Error('el todo es obligatorio')
 
 
+    //desestructuramos el todo
+    const { done, description, id } = todo
+
     //creamos el html para el todo
-    const html = `<h3>${todo.description}</h3>`
+    const html = `  
+                        <div class="view">
+                            <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+                            <label>${description}</label>
+                            <button class="destroy"></button>
+                        </div>                        
+                   
+                    `
 
     const liTodo = document.createElement('li')
-
     liTodo.innerHTML = html
+    //le agregamos atributos al html creado
+    liTodo.setAttribute('data-id', id)
+
+    //ponemos la clase completed si es que lo esta
+    done ? liTodo.classList.add('completed') : ''
 
     return liTodo
 }
--- 05-todo-app/src/todos/use-cases/render-todos.js
+++ 05-todo-app/src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from "../models/todo.model";
 import { createTodoHtml } from "./";
 
+//creamos esta variable para guardar el elemento hmtl que se va a buscar
+let element;
 
 /**
  * funcion para renderizar los todos, recibe como parametro el identificador html y los todos que quieren renderizar
@@ -9,8 +11,14 @@ import { createTodoHtml } from "./";
  */
 export const renderTodos = (elementId, todos = []) => {
 
-    //obtenemos el elemento html del dom
-    const element = document.querySelector(elementId)
+    //si la variable element esta vacia entonces buscamos el elemento html en el DOM
+    if (!element) element = document.querySelector(elementId);
+    //si luego de buscar el elemento sigue vacia la variable es que mandaron mal el parametro
+    if (!element) throw new Error('el elementId enviado no lo encontro, por favor mandar correctamente ura')
+
+
+    //aqui purgamos o limpiamos todo el contenido para que no se apile
+    element.innerHTML = '';
 
     //barremos los todos y llamamos al caso de uso
     todos.forEach(todo => {
